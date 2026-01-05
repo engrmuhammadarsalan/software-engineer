266fe3c82029ff07eb48b93b50851d6b4404571e
engrmuhammadarsalan <muhammadarsh68@gmail.com>
2026-01-06 00:02:17 +0500

    more

--- src/RcFun.java
+++ src/RcFun.java
@@ -1,19 +1,19 @@
+import java.util.Scanner;
+
 public class RcFun {
 
     public static void Num(int n){
         if (n==0){
             return;
         }
-        System.out.println(n);
         Num(n-1);
+        System.out.println(n);
     }
-
-
-
-
-
     public static void main (String[] args){
-        int n = 5;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The values");
+        int n = sc.nextInt();
         Num(n);
     }
 }
