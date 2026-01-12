5894fb4a75d61539bf716a9786c99ac329913f14
engrmuhammadarsalan <muhammadarsh68@gmail.com>
2026-01-12 17:14:52 +0500

    more

---
+++ src/Inheritence9348.java
@@ -0,0 +1,25 @@
+class Vehical{
+    int speed;
+
+    public void move(){
+        System.out.println("Car Is Moving!");
+    }
+}
+class Carr extends Vehical{
+
+    public void playMusic(){
+        System.out.println("Music IS Playing");
+    }
+}
+
+public class Inheritence9348 {
+
+    public static void main (String[] args){
+        Vehical v1 = new Vehical();
+        v1.move();
+
+        Carr c1 = new Carr();
+        c1.playMusic();
+
+    }
+}
---
+++ src/inheritenceTopic.java
@@ -0,0 +1,16 @@
+class Shape{
+    String color;
+}
+
+class Triangle extends Shape{
+
+}
+
+public class inheritenceTopic {
+
+    public static void main (String[] args){
+        Triangle T1 = new Triangle();
+        T1.color = "red";
+
+    }
+}
