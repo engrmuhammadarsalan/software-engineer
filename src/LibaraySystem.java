e6e9f51e75968cdca7bc4abd5791457439287d10
engrmuhammadarsalan <muhammadarsh68@gmail.com>
2026-01-20 17:11:16 +0500

    more

--- src/LibaraySystem.java
+++ src/LibaraySystem.java
@@ -1,38 +1,49 @@
-class Books{
-    private String tittle;
+class Books {
+    private String title;
     private String author;
-    private boolean issue;
+    private boolean isIssued;
 
-    Books(String tittle, String author, Boolean issue){
-        this.tittle = tittle;
+    // Constructor
+    Books(String title, String author) {
+        this.title = title;
         this.author = author;
-        this.issue = false;
+        this.isIssued = false;
     }
 
-    public void setTittle(){
-        this.tittle = tittle;
-    }
-    public void setAuthor(){
-        this.author = author;
-    }
-    public String getTittle(){
-        return tittle;
-    }
-    public String getAuthor(){
-        return author;
-    }
-
-
-    public void isIssue(){
-        if (boolean issue){
-
+    public void issueBook() {
+        if (isIssued) {
+            System.out.println("Book already issued");
+        } else {
+            isIssued = true;
+            System.out.println("Book issued successfully");
         }
+    }
+
+    public void returnBook() {
+        if (!isIssued) {
+            System.out.println("Book was not issued");
+        } else {
+            isIssued = false;
+            System.out.println("Book returned successfully");
+        }
+    }
+
+    public void showDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Issued: " + isIssued);
     }
 }
 
 public class LibaraySystem {
+    public static void main(String[] args) {
 
-    public static void main (String[] args){
-        Books b2 = new Books("Avengers", "Robert");
+        Books b1 = new Books("Avengers", "Robert");
+
+        b1.showDetails();
+        b1.issueBook();
+        b1.issueBook();
+        b1.returnBook();
+        b1.showDetails();
     }
 }
